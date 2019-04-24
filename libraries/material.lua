dd2835be09e81395377e10fcc967cdd46796b4a0
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-04-24 17:30:14 +0300

    Added draft for add_metal()

--- libraries/material.lua
+++ libraries/material.lua
@@ -1,6 +1,19 @@
 -- A library that includes functions to make materials, such as metals, and unified recipes for them
 local materiallib
 
-materiallib
+materiallib.add_metal = function(parameters)
+
+end
+
+local metal_example = {
+    name = "iron",
+    heat_capacity = 450, -- J / (kg * degree (Celsius))
+    melting_temperature = 1538, -- Celsius
+    prototypes = {
+        item = "iron-plate",
+        fluid = "molten-iron",
+        item-to-fluid = 50 -- How much molten metal you will get for 1 item
+    },
+}
 
 return materiallib
