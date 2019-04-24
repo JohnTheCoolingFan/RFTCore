6a4db7872059cad611bd9c09822633c337d15a89
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-04-24 18:11:29 +0300

    Added add_metal() function

--- libraries/material.lua
+++ libraries/material.lua
@@ -2,7 +2,37 @@
 local materiallib
 
 materiallib.add_metal = function(parameters)
+    local output = {
+        {
+            type = "recipe",
+            name = parameters.name.."-melting",
+            ingredients = {{parameters.prototypes.item, parametrs.prototypes.item_to_fluid[1]},
+            results = {{type = "fluid", name = parameters.prototypes.fluid, amount = parameters.prototypes.item_to_fluid[2]}},
+            energy_required = math.ceil(parameters.heat_capacity * parameters.melting_temperature / 80000 * settings.startup["RFT-metal-melting-time-modifier"]) * 10
+        },
+        {
+            type = "recipe",
+            name = "molten-"..parameters.name.."-casting",
+            category = "RFT-metal-casting"
+            ingredients = {{type = "fluid", name = parameters.prototypes.fluid, amount = parameters.prototypes.item_to_fluid[2]}},
+            result = parameters.prototypes.item,
+            result_count = parameters.prototypes.item_to_fluid[1],
+            energy_required = math.ceil(parameters.heat_capacity * parameters.melting_temperature / 80000 * settings.startup["RFT-metal-melting-time-modifier"]) * 10
+        }
+    }
 
+    -- Melting category
+    if parameters.melting_temperature < settings.startup["RFT-metal-melting-t1-max-temperature"].value then
+        output[1].category = "RFT-metal-melting-t1"
+    elseif parameters.melting_temperature < settings.startup["RFT-metal-melting-t2-max-temperature"].value then
+        output[1].category = "RFT-metal-melting-t2"
+    elseif parameters.melting_temperature < settings.startup["RFT-metal-melting-t3-max-temperature"].value then
+        output[1].category = "RFT-metal-melting-t3"
+    else
+        output[1].category = "RFT-metal-melting-t4"
+    end
+
+    data:extend(output)
 end
 
 local metal_example = {
@@ -12,7 +42,7 @@ local metal_example = {
     prototypes = {
         item = "iron-plate",
         fluid = "molten-iron",
-        item-to-fluid = 50 -- How much molten metal you will get for 1 item
+        item_to_fluid = {5, 500} -- How much molten metal [2] you will get for [1] item
     },
 }
 
