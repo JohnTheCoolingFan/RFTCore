3d45de89c69be205294f3821c0cd44ed2c0cf30d
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-04-24 17:23:47 +0300

    Added metal smelting categories

---
+++ prototypes/categories/recipe-category.lua
@@ -0,0 +1,14 @@
+data:extend({
+    {
+        type = "recipe-category",
+        name = "metal-smelting-t1"
+    },
+    {
+        type = "recipe-category",
+        name = "metal-smelting-t2"
+    },
+    {
+        type = "recipe-category",
+        name = "metal-smelting-t3"
+    }
+})
