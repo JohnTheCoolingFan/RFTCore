61c5b216da542e1192493de8d701430a4119bda6
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-04-24 17:42:42 +0300

    Added melting time modifier setting

--- settings.lua
+++ settings.lua
@@ -12,12 +12,18 @@ data:extend({
         setting_type = "startup",
         default_value = 1600, -- Iron melting IRL
         minimal_value = 100 -- Water boiling
-    }
+    },
     {
         name = "RFT-metal-melting-t3-max-temperature",
         type = "int-setting",
         setting_type = "startup",
         default_value = 3500, -- Tungsten melting IRL
         minimal_value = 100 -- Water boiling
+    },
+    {
+        name = "RFT-metal-melting-time-modifier",
+        type = "double-setting",
+        setting_type = "startup",
+        default_value = 1
     }
 })
