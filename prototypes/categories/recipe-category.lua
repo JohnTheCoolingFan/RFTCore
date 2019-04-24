3dc8ea96dfbef33df3bf90846fda52aa402f056e
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-04-24 17:48:01 +0300

    renamed metal melting recipe categories

--- prototypes/categories/recipe-category.lua
+++ prototypes/categories/recipe-category.lua
@@ -1,14 +1,14 @@
 data:extend({
     {
         type = "recipe-category",
-        name = "metal-melting-t1"
+        name = "RFT-metal-melting-t1"
     },
     {
         type = "recipe-category",
-        name = "metal-melting-t2"
+        name = "RFT-metal-melting-t2"
     },
     {
         type = "recipe-category",
-        name = "metal-melting-t3"
+        name = "RFT-metal-melting-t3"
     }
 })
